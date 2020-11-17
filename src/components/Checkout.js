88c69130293eb2b6e3795ae4caf383f21a31a7c9
Ismail Al Kamal <ismail@nawatt.com>
2020-11-17 17:03:15 +0300

    Checkout done

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,38 +1,65 @@
 import React from "react";
 import { useStateValue } from "../contexts/StateContext";
+import Subtotal from "./Subtotal";
+import "./Checkout.css";
 
 function Checkout() {
-  const [{ basket, total }, dispatch] = useStateValue();
+  const [{ basket }, dispatch] = useStateValue();
 
   const removeItem = (product) => {
     dispatch({ type: "REMOVE_FROM_BASKET", payload: product });
   };
 
   return (
-    <div>
-      <div>
-        <p>Total: AED {total}</p>
-      </div>
-      {basket.map((item) => (
-        <div key={item.id} className="product">
-          <div className="product__info">
-            <p>{item.title}</p>
-            <p className="product__price">
-              <small>AED </small>
-              <strong>{item.price}</strong>
-            </p>
-            <div className="product__rating">
-              {Array(Math.floor(item.rating))
-                .fill()
-                .map((_, i) => (
-                  <p key={i}>⭐</p>
-                ))}
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://wordstream-files-prod.s3.amazonaws.com/s3fs-public/styles/simple_image/public/images/media/images/banner-ads-examples-disney%2B.jpg?so0uQt68ZOFb5FN0rSzSWtfGinew87_K&itok=kJxsCgPs"
+          alt=""
+        />
+
+        {basket.length === 0 ? (
+          <div>
+            <h2>Your Shopping Basket is empty</h2>
+          </div>
+        ) : (
+          <div className="checkout__title">
+            <h2>Your Shopping Basket</h2>
+          </div>
+        )}
+        {basket.map((item) => (
+          <div key={item.id} className="checkoutProduct">
+            <img className="checkoutProduct__image" src={item.image} alt="" />
+            <div className="checkoutProduct__info">
+              <p className="checkoutProduct__title">{item.title}</p>
+              <p className="checkoutProduct__price">
+                <small>AED </small>
+                <strong>{item.price}</strong>
+              </p>
+              <div className="checkoutProduct__rating">
+                {Array(Math.floor(item.rating))
+                  .fill()
+                  .map((_, i) => (
+                    <p key={i}>⭐</p>
+                  ))}
+              </div>
+              <button onClick={() => removeItem(item)}>
+                Remove from basket
+              </button>
             </div>
           </div>
-          <img src={item.image} alt="" />
-          <button onClick={() => removeItem(item)}>Remove from basket</button>
+        ))}
+      </div>
+      {basket.length ? (
+        <div className="checkout__right">
+          <Subtotal />
         </div>
-      ))}
+      ) : (
+        <div>
+          <p>You still don't have any items in your basket.</p>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/components/Subtotal.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useStateValue } from "../contexts/StateContext";
+import CurrencyFormat from "react-currency-format";
+import "./Subtotal.css";
+
+function Subtotal() {
+  const [{ basket, total }, dispatch] = useStateValue();
+
+  return (
+    <div className="subtotal">
+      <div className="subtotal__price">
+        <CurrencyFormat
+          renderText={(value) => {
+            return (
+              <>
+                <p>
+                  Subtotal ({basket.length} items): <strong>{value}</strong>
+                </p>
+                <small className="subtotal__gift">
+                  <input type="checkbox" /> This order contains a gift
+                </small>
+              </>
+            );
+          }}
+          decimalScale={2}
+          value={total}
+          displayType="text"
+          thousandSeparator={true}
+          prefix={"AED "}
+        />
+      </div>
+      <button className="subtotal__button">Procced to Checkout</button>
+    </div>
+  );
+}
+
+export default Subtotal;
