0907e427144c3402a00345ab3ea22f5d552f7626
Ismail Al Kamal <ismail@nawatt.com>
2020-11-17 17:23:59 +0300

    Added logo to Login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,15 @@ import { Link, useHistory } from "react-router-dom";
 
 function Login() {
   return (
-    <div>
+    <div className="Login">
+      <Link to="/">
+        <img
+          className="login_logo"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1920px-Amazon_logo.svg.png"
+          alt=""
+        />
+      </Link>
+
       <h1>Login</h1>
     </div>
   );
